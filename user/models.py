e2a28571a2cf2228ba5fe62468135139432ee25a
muhammad zulfikar <muhammad.zulfikar1593@gmail.com>
2024-02-07 12:54:06 +0700

    fix: circular dependency and black

--- user/models.py
+++ user/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class UserTypeChoices(models.TextChoices):
-    OWNER = ('OWNER', 'OWNER')
-    EMPLOYEE = ('EMPLOYEE', 'EMPLOYEE')
+    OWNER = ("OWNER", "OWNER")
+    EMPLOYEE = ("EMPLOYEE", "EMPLOYEE")
+
 
 class User(AbstractUser):
-    company = models.ForeignKey('company.Company', on_delete=models.CASCADE, related_name='employee', null=True, blank=True)
-    user_type = models.CharField(max_length=10, null=False, blank=False, choices=UserTypeChoices.choices)
+    company = models.ForeignKey(
+        "company.Company",
+        on_delete=models.CASCADE,
+        related_name="employee",
+        null=True,
+        blank=True,
+    )
+    user_type = models.CharField(
+        max_length=10, null=False, blank=False, choices=UserTypeChoices.choices
+    )
