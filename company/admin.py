3f58c30a5d184f8b0ca4b73d2cd214c160d374bd
izuleon <izuleonel@gmail.com>
2024-02-11 13:21:22 +0700

    feat: adminpage user and company (#5)
    Co-authored-by: muhammad zulfikar <muhammad.zulfikar1593@gmail.com>

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,19 @@
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from company.models import Company
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+        "created_by",
+    ]
+    readonly_fields = [
+        "created_by",
+        "updated_by",
+    ]
+
+
+admin.site.register(Company, CompanyAdmin)
