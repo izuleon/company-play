245b91b6ea7a543b063ededce3f1eddedd66aa0d
muhammad zulfikar <muhammad.zulfikar1593@gmail.com>
2024-02-11 11:41:07 +0700

    feat: company creations

--- company/views.py
+++ company/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
-
 # Create your views here.
+from drf_spectacular.utils import extend_schema
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from company.models import Company
+from company.serializer import CompanyCreationSerializer
+from user.models import User, UserTypeChoices
+from utils.permission import IsOwner
+
+
+class CompanyCreationView(APIView):
+    permission_classes = [IsOwner]
+
+    @extend_schema(
+        request=CompanyCreationSerializer,
+    )
+    def post(self, request):
+        serializer = CompanyCreationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        name = serializer.validated_data.get("name")
+        industry = serializer.validated_data.get("industry")
+
+        new_company = Company.objects.create(
+            name=name,
+            industry=industry,
+            created_by=request.user,
+        )
+        return Response(
+            {"message": f"Company {new_company.name} created"},
+            status=status.HTTP_200_OK,
+        )
--- user/serializer.py
+++ user/serializer.py
@@ -1,20 +1,10 @@
 # apps/your_app/serializers.py
 from rest_framework import serializers
 
-from company.models import IndustryChoices
-
 
 class TokenSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=255)
     password = serializers.CharField(max_length=128, write_only=True)
-
-
-def company_industry_validator(value):
-    if value not in IndustryChoices:
-        raise serializers.ValidationError(
-            f"Invalid industry choice, try use {[names[0] for names in IndustryChoices.choices]}"
-        )
-    return value
 
 
 class OwnerSignUpSerializer(serializers.Serializer):
@@ -23,7 +13,3 @@ class OwnerSignUpSerializer(serializers.Serializer):
     first_name = serializers.CharField(max_length=255, default=None)
     last_name = serializers.CharField(max_length=255, default=None)
     email = serializers.CharField(max_length=255, default=None)
-    company_name = serializers.CharField(max_length=255)
-    company_industry = serializers.CharField(
-        max_length=100, validators=[company_industry_validator]
-    )
