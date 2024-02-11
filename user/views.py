a14f546f0aff0637e6a77a56bafc07d07fd3ca11
izuleon <izuleonel@gmail.com>
2024-02-11 11:52:28 +0700

    feat: register employee (#4)
    Co-authored-by: muhammad zulfikar <muhammad.zulfikar1593@gmail.com>

--- user/views.py
+++ user/views.py
@@ -8,10 +8,11 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 from test_case import settings
 from user.models import User, UserTypeChoices
-from user.serializer import OwnerSignUpSerializer, TokenSerializer
+from user.serializer import SignUpSerializer, TokenSerializer
+from utils.permission import IsOwner
 
 
-class TokenObtainView(APIView):
+class UserLoginView(APIView):
     @extend_schema(
         request=TokenSerializer,
     )
@@ -36,10 +37,10 @@ class TokenObtainView(APIView):
 
 class OwnerSignUpView(APIView):
     @extend_schema(
-        request=OwnerSignUpSerializer,
+        request=SignUpSerializer,
     )
     def post(self, request):
-        serializer = OwnerSignUpSerializer(data=request.data)
+        serializer = SignUpSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         username = serializer.validated_data.get("username")
@@ -56,6 +57,38 @@ class OwnerSignUpView(APIView):
             user_type=UserTypeChoices.OWNER,
         )
         new_user.set_password(password)
+        new_user.save()
+        return Response(
+            {"message": f"User {new_user.get_full_name()} created"},
+            status=status.HTTP_200_OK,
+        )
+
+
+class RegisterEmployeeView(APIView):
+    permission_classes = [IsOwner]
+
+    @extend_schema(
+        request=SignUpSerializer,
+    )
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        username = serializer.validated_data.get("username")
+        password = serializer.validated_data.get("password")
+        first_name = serializer.validated_data.get("first_name")
+        last_name = serializer.validated_data.get("last_name")
+        email = serializer.validated_data.get("email")
+
+        new_user = User.objects.create(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            user_type=UserTypeChoices.EMPLOYEE,
+        )
+        new_user.set_password(password)
+        new_user.company = request.user.company
         new_user.save()
         return Response(
             {"message": f"User {new_user.get_full_name()} created"},
